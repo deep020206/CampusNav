231889966b8da51e45e268014575ddc711342216
deep020206 <23it060@charusat.edu.in>
2026-04-03 10:40:21 +0530

    Fix: Implement proper route generation handler for dynamic routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'models/location_model.dart';
 import 'providers/navigation_provider.dart';
 import 'screens/auth/login_screen.dart';
 import 'screens/auth/signup_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/location_details_screen.dart';
+import 'screens/locations_list_screen.dart'; // ignore: unused_import
 import 'screens/navigation_screen.dart';
 import 'screens/settings_screen.dart';
 import 'theme/app_theme.dart';
@@ -14,6 +17,36 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Route<dynamic> _onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/login':
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case '/signup':
+        return MaterialPageRoute(builder: (_) => const SignupScreen());
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case '/navigation':
+        return MaterialPageRoute(builder: (_) => const NavigationScreen());
+      case '/settings':
+        return MaterialPageRoute(builder: (_) => const SettingsScreen());      case '/locations':
+        final args = settings.arguments as LocationType?;
+        return MaterialPageRoute(
+          // ignore: undefined_method
+          builder: (_) => LocationsListScreen(locationType: args),
+        );
+      case '/location-details':
+        final location = settings.arguments as CampusLocation?;
+        if (location != null) {
+          return MaterialPageRoute(
+            builder: (_) => LocationDetailsScreen(location: location),
+          );
+        }
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +60,7 @@ class MyApp extends StatelessWidget {
         darkTheme: AppTheme.darkTheme,
         themeMode: ThemeMode.light,
         home: const LoginScreen(),
-        routes: {
-          '/login': (context) => const LoginScreen(),
-          '/signup': (context) => const SignupScreen(),
-          '/home': (context) => const HomeScreen(),
-          '/navigation': (context) => const NavigationScreen(),
-          '/settings': (context) => const SettingsScreen(),
-        },
+        onGenerateRoute: _onGenerateRoute,
       ),
     );
   }
